494eece13f1bc8ae462b6e4ff61bce8d6c33d77d
sujunhao <spike76@qq.com>
2020-08-28 17:45:34 +0800

    refactor

--- lib/proxy_beggar.rb
+++ lib/proxy_beggar.rb
@@ -1,24 +1,25 @@
 require_relative './proxy_beggar/valid_proxy_pool'
+require_relative './proxy_beggar/config'
 
 class ProxyBeggar
 
   def beg(crawlers = load_crawlers)
-    pool = ValidProxyPool.instance
     Thread.new do
+      pool = ValidProxyPool.instance
       loop do
-        sleep 10
+        sleep Config[:begger][:clean_gap_time]
         pool.hard_clear_invalid_proxies
       end
     end
 
-    loop do
-      Array(crawlers).each_with_object([]) do |crawler, threads|
-        threads << Thread.new do
+    Array(crawlers).each_with_object([]) do |crawler, threads|
+      threads << Thread.new do
+        loop do
           crawler.run
+          sleep Config[:begger][:crawl_gap_time]
         end
-      end.each(&:join)
-      sleep 10
-    end
+      end
+    end.each(&:join)
   end
 
   private
