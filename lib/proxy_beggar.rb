36655e9b73161e5cc70dc1ed471bd7e690a2322e
sjh <spike76@qq.com>
2020-10-14 15:12:19 +0800

    detect website is ok

--- lib/proxy_beggar.rb
+++ lib/proxy_beggar.rb
@@ -3,7 +3,7 @@ require_relative './proxy_beggar/config'
 
 class ProxyBeggar
 
-  def beg(crawlers = load_crawlers)
+  def beg(crawlers = ProxyBeggar.load_crawlers)
     Thread.new do
       pool = ValidProxyPool.instance
       loop do
@@ -22,20 +22,18 @@ class ProxyBeggar
     end.each(&:join)
   end
 
-  private
-
-    def load_crawlers
-      Dir['./proxy_beggar/crawlers/*.rb'].each_with_object([]) do |path, objs|
-        unless path.match?('base_crawler')
-          require_relative path
-          file_name = path.match(/(\w+)\.rb/)[1]
-          klass = self.class.const_get(file_name.split('_').map(&:capitalize).join(''))
-          objs << klass.new
-        end
+  def self.load_crawlers
+    Dir['./proxy_beggar/crawlers/*.rb', base: __dir__].each_with_object([]) do |path, objs|
+      unless path.match?('base_crawler')
+        require_relative path
+        file_name = path.match(/(\w+)\.rb/)[1]
+        klass = self.const_get(file_name.split('_').map(&:capitalize).join(''))
+        objs << klass.new
       end
     end
+  end
 
 end
-#require_relative './proxy_beggar/crawlers/sevenyip_crawler'
-#ProxyBeggar.new.beg(ProxyBeggar::SevenyipCrawler.new)
-ProxyBeggar.new.beg
+# require_relative './proxy_beggar/crawlers/sevenyip_crawler'
+# ProxyBeggar.new.beg(ProxyBeggar::SevenyipCrawler.new)
+# ProxyBeggar.new.beg
